39584ea27517d6caba610bca7b579fa9bdfaa18d
uncodem <emurex185@gmail.com>
2026-04-04 22:45:32 +0800

    Added text area page and NotesList component

---
+++ app/_components/NotesList.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function NotesList() {
+    return <h1>Note Entry</h1>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
-import Image from "next/image";
+'use client';
+import {useEffect, useState} from "react";
+import NotesList from "./_components/NotesList";
 
-export default function Home() {
+export default function Page() {
+
+  const [contents, setContent] = useState("");
+  const [debounced, setDebounced] = useState(contents);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebounced(contents)
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [contents]);
+
+  useEffect(() => {
+    if (!debounced) return
+    console.log("Autosave: ", debounced);
+  }, [debounced]);
+
   return (
-    <h1 className="text-lg">Home</h1>
+    <div className="grid grid-cols-[250px_1fr] h-screen">
+      <div className="border-r">
+        <NotesList />
+      </div>
+      <div>
+        <textarea className="w-full h-full p-4" value={contents} onChange={(e) => setContent(e.target.value)}/>
+      </div>
+    </div>
   );
 }
